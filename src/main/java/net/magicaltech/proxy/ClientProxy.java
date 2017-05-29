8e394dceef43e06370bbc75f721d626807aeb86a
XFireEyeZ <h.filip.holmberg@gmail.com>
2017-05-29 16:14:08 +0200

    Registering renders
    Added registering of renders in the clientproxy

--- src/main/java/net/magicaltech/proxy/ClientProxy.java
+++ src/main/java/net/magicaltech/proxy/ClientProxy.java
@@ -1,10 +1,14 @@
 package net.magicaltech.proxy;
 
+import net.magicaltech.init.ModBlocks;
+import net.magicaltech.init.ModItems;
+
 public class ClientProxy extends CommonProxy {
 	
 	@Override
 	public void registerRenders() {
-		
+		ModItems.registerRenders();
+		ModBlocks.registerRenders();
 	}
 
 }
