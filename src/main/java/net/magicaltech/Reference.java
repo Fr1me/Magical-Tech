436594f110cdc80ea8b86ecf9e35b23ab98ccda7
sindrefag <sindrefagerheim53@gmail.com>
2017-06-22 20:30:18 +0200

    Fixed something?

--- src/main/java/net/magicaltech/Reference.java
+++ src/main/java/net/magicaltech/Reference.java
@@ -5,7 +5,7 @@ public class Reference {
 	public static final String MODID = "magicaltech";
 	public static final String NAME = "Magical Tech";
 	public static final String VERSION = "0.0.4";
-	public static final String MC_VERSION = "1.12";
+	public static final String MC_VERSION = "[1.12]";
 	public static final String SERVER_PROXY_CLASS = "net.magicaltech.proxy.CommonProxy";
 	public static final String CLIENT_PROXY_CLASS = "net.magicaltech.proxy.ClientProxy";
 
